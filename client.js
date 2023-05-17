227f4aef1437fa83e7bb590fa8fd673d66b5b2bc
Sofia Dersén <81019781+sdersen@users.noreply.github.com>
2023-05-17 21:19:03 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "0gjhdiu9", //Insert your Id here.
+  projectId: "xxxxxxx", //Insert your Id here.
   dataset: "production", //Insert your dataset here.
   apiVersion: "2022-03-25",
   useCdn: false,

042a5c850652c34909a36079bb3f1ee3675032a3
Sofia Dersén <81019781+sdersen@users.noreply.github.com>
2023-05-17 19:45:22 +0200

    Boilerplate

---
+++ client.js
@@ -0,0 +1,8 @@
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: "xxxxxx", //Insert your Id here.
+  dataset: "production", //Insert your dataset here.
+  apiVersion: "2022-03-25",
+  useCdn: false,
+});
